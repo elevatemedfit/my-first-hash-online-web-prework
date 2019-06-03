43a444d3e54919d32bfd374601087b098f5839b5
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-03 20:15:15 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,8 +36,9 @@ def adding
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+shipping_manifest["gun powder"] = 2
 
   # return the shipping_manifest hash below
 
 end
+return shipping_manifest
